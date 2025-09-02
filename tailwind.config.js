160466a7585fa513bbd746c19465dbcb73cbeaef
Cristhianm30 <Cristhian.452@outlook.com>
2025-09-02 17:51:07 -0500

    component projects,education done

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: [
-    "./src/**/*.{html,ts}",
-  ],
+  content: ['./src/**/*.{html,ts}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        heikoh: {
+          beige: 'var(--color-beige-heikoh)',
+          light: 'var(--color-blue-light-heikoh)',
+          dark: 'var(--color-blue-dark-heikoh)',
+        },
+      },
+    },
   },
   plugins: [],
-}
-
+};
